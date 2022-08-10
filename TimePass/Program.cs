fcd3cef24076c45540087cc6f89c9546addca7a5
sajeed-syn <sajeedrs@gmail.com>
2022-08-10 20:49:55 +0530

    Add project files.

---
+++ TimePass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimePass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
